31481ed118057e3c2835257c073d4f70c3d79569
Imranshahadat95 <imranshahadat95@gmail.com>
2018-08-12 14:09:47 +0600

    my first commit
    commit to master

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*------------------
+		TYPED JS
+	--------------------*/
+	$(".element").typed({
+		strings: ["I'm Imran Shahadat", "Front-End Web Developer", "From Dhaka,Bangladesh"],
+		typeSpeed: 10,
+		loop:true,
+		backDelay: 2000
+	});
